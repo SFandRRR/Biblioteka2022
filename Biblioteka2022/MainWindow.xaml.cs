5148f35ad25465777bdf06c2b8b3e7a1e2c6faee
r <stas.frak@gmail.com>
2022-11-22 14:18:31 +0100

    Wyswietlanie dziala super

--- Biblioteka2022/MainWindow.xaml.cs
+++ Biblioteka2022/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace Biblioteka2022
 
             pola_tabeli(tabela); 
 
-            string z = wyswietl_rekordy(tabela, "", "", 0);
+            string z = wyswietl_rekordy(tabela, "", "", "");
             Trace.WriteLine(z + "bbbbb");
 
         }
@@ -32,10 +32,11 @@ namespace Biblioteka2022
         private void button_wyswietl_szukaj_Click(object sender, RoutedEventArgs e)
         {
             string tabela = "";
-            string pole = "";
-            string filtr = "";           
-            int sposob = 0;
+            string pole = "";                  
+            string sposob = "";
+            string filtr = "";
 
+            filtr = input_wyswietl_filtr.Text.ToString();
             string value = "";
 
             ComboBoxItem ItemTabela = (ComboBoxItem)input_wyswietl_tabela.SelectedItem;
@@ -43,18 +44,18 @@ namespace Biblioteka2022
                 tabela = ItemTabela.Content.ToString();
             }
 
-            /*ComboBoxItem ItemPole = (ComboBoxItem)input_wyswietl_pole.SelectedItem;
-            if (ItemPole != null)
-            {
-                pole = ItemPole.Content.ToString();
-            }*/
-
             if (input_wyswietl_pole.SelectedItem != null)
             {
                 pole = input_wyswietl_pole.SelectedItem.ToString();
             }
 
-            value = wyswietl_rekordy(tabela, pole, "", 0);
+            ComboBoxItem ItemSposob = (ComboBoxItem)input_wyswietl_sposob.SelectedItem;
+            if (ItemSposob != null)
+            {
+                sposob = ItemSposob.Content.ToString();          
+            }
+
+            value = wyswietl_rekordy(tabela, pole, sposob, filtr);
 
             textbox_wyswietl.Document.Blocks.Clear();
             textbox_wyswietl.Document.Blocks.Add(new Paragraph(new Run(value.ToString())));
@@ -65,60 +66,14 @@ namespace Biblioteka2022
         string wyswietl_rekordy(
            string tabela = "",
            string pole = "",
-           string filtr = "",
-           int sposob = 0)
+           string sposob = "",
+           string filtr = ""
+           )
         {
-            string Query = "SELECT ";
+            string Query = "SELECT * ";
             string TheInformation = "";
             if (tabela != "")
-            {
-
-                switch (pole)
-                {
-                    case "Id":
-                        Query += "Id ";
-                        break;
-                    case "Id Klienta":
-                        Query += "Id_Klient ";
-                        break;
-                    case "Id Ksiązki":
-                        Query += "Id_Ksiazka ";
-                        break;
-                    case "Data Wyporzyczenia":
-                        Query += "DataWyporzyczenia ";
-                        break;
-                    case "Data Zwrotu":
-                        Query += "DataZwrotu ";
-                        break;
-                    case "Tytuł":
-                        Query += "Tytul ";
-                        break;
-                    case "Autor":
-                        Query += "Autor ";
-                        break;
-                    case "Opis":
-                        Query += "Opis ";
-                        break;
-                    case "Rok Wydania":
-                        Query += "RokWydania ";
-                        break;
-                    case "Imie":
-                        Query += "Imie ";
-                        break;
-                    case "Nazwisko":
-                        Query += "Nazwisko ";
-                        break;
-                    case "Pesel":
-                        Query += "Pesel ";
-                        break;
-                    case "Telefon":
-                        Query += "Telefon ";
-                        break;
-
-                    default:
-                        Query += "* ";
-                        break;
-                }
+            {               
 
                 switch (tabela)
                 {
@@ -138,9 +93,78 @@ namespace Biblioteka2022
                         return "Nie wybrano tabeli";
                         break;
                 }
-                if (sposob != 0)
-                {
 
+                switch (pole)
+                {
+                    case "Id":
+                        Query += " WHERE Id ";
+                        break;
+                    case "Id Klienta":
+                        Query += " WHERE Id_Klient ";
+                        break;
+                    case "Id Ksiązki":
+                        Query += " WHERE Id_Ksiazka ";
+                        break;
+                    case "Data Wyporzyczenia":
+                        Query += " WHERE DataWyporzyczenia ";
+                        break;
+                    case "Data Zwrotu":
+                        Query += " WHERE DataZwrotu ";
+                        break;
+                    case "Tytuł":
+                        Query += " WHERE Tytul ";
+                        break;
+                    case "Autor":
+                        Query += " WHERE Autor ";
+                        break;
+                    case "Opis":
+                        Query += " WHERE Opis ";
+                        break;
+                    case "Rok Wydania":
+                        Query += "WHERE RokWydania ";
+                        break;
+                    case "Imie":
+                        Query += " WHERE Imie ";
+                        break;
+                    case "Nazwisko":
+                        Query += " WHERE Nazwisko ";
+                        break;
+                    case "Pesel":
+                        Query += " WHERE Pesel ";
+                        break;
+                    case "Telefon":
+                        Query += " WHERE Telefon ";
+                        break;
+
+                    default:
+                        Query += "";
+                        break;
+                }
+
+                if (filtr == "")
+                {
+                    filtr = "0";
+                }
+
+                if (pole != "[Brak]") {
+
+                switch (sposob)
+                    {
+                        case "Rozpoczyna się od":
+                        
+                            break;
+                        case "Jest równe":
+                            Query += "= "+filtr.ToString();
+                            break;
+                        case "Zawiera":
+
+                            break;
+
+                        default:
+                            Query += " = 0 ";
+                            Trace.WriteLine("> This code should not be reachable switch(sposob)");
+                            break;
+                    }
                 }
                 Query += ";";
 
@@ -157,7 +181,7 @@ namespace Biblioteka2022
                     {
                         if (reader.Read())
                         {
-                            TheInformation += reader[0] + " | " + reader[1] + " | " + reader[2] + " | " + reader[3].ToString().Substring(0, 10) + " | " + reader[4].ToString().Substring(0,10) + "\n";
+                            TheInformation += reader[0] + " | " + reader[1] + " | " + reader[2] + " | " + reader[3] + " | " + reader[4] + "\n";
                         }
                     }
 
@@ -183,6 +207,7 @@ namespace Biblioteka2022
                 case "Wypożyczenia":
                     if(input_wyswietl_pole != null) { 
                         input_wyswietl_pole.Items.Clear();
+                        input_wyswietl_pole.Items.Add("[Brak]");
                         input_wyswietl_pole.Items.Add("Id");
                         input_wyswietl_pole.Items.Add("Id Klienta");
                         input_wyswietl_pole.Items.Add("Id Książki");
@@ -196,6 +221,7 @@ namespace Biblioteka2022
                     if (input_wyswietl_pole != null)
                     {
                         input_wyswietl_pole.Items.Clear();
+                        input_wyswietl_pole.Items.Add("[Brak]");
                         input_wyswietl_pole.Items.Add("Id Książki");
                         input_wyswietl_pole.Items.Add("Tytuł");
                         input_wyswietl_pole.Items.Add("Autor");
@@ -209,6 +235,7 @@ namespace Biblioteka2022
                     if (input_wyswietl_pole != null)
                     {
                         input_wyswietl_pole.Items.Clear();
+                        input_wyswietl_pole.Items.Add("[Brak]");
                         input_wyswietl_pole.Items.Add("Id Klienta");
                         input_wyswietl_pole.Items.Add("Imie");
                         input_wyswietl_pole.Items.Add("Nazwisko");
